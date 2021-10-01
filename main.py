9d43d469d7c09191085cdf588d2bba40514d81fd
prachidewangan <73733161+prachidewangan@users.noreply.github.com>
2021-10-01 17:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import csv
 from cvzone.HandTrackingModule import HandDetector
 import cvzone
 import time
+capture = cv2.VideoCapture(0)
+capture.set(3,1280)
+capture.set(4,720)
+
+
+while True:
+    sucess,image = capture.read()
+    cv2.imshow('frame',image)
+    cv2.waitKey(1)
